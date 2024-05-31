425cdaf84e169a2d897207985a18fe7ab75ac911
fawwazmak <fawwazmakinde47@gmail.com>
2024-05-31 10:29:17 +0100

    responsiveness adjusted

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let hourHand = document.getElementById("hour");
 let digitalHour = document.getElementById("digital-hour");
 let digitalMinute = document.getElementById("digital-minute");
 let digitalSecond = document.getElementById("digital-second");
+// console.log(myScreenWidth);
 
 const countSeconds = () => {
   const date = new Date();
@@ -12,7 +13,7 @@ const countSeconds = () => {
 
   secondHand.style.transform = `rotate(${secondDegree}deg)`;
 
-  digitalSecond.innerHTML = seconds;
+  digitalSecond.innerHTML = seconds.toString().padStart(2, "0");
 };
 
 setInterval(countSeconds, 1000);
@@ -23,17 +24,29 @@ function countMinutes() {
   let minutesDegree = (minutes / 60) * 360 + 90;
   minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
 
-  digitalMinute.innerHTML = minutes + ":";
+  
+  let myScreenWidth = window.screen.width;
+  if (myScreenWidth > 240) {
+    digitalMinute.innerHTML = minutes.toString().padStart(2, "0") + ":";
+  } else {
+    digitalMinute.innerHTML = minutes.toString().padStart(2, "0");
+  }
 }
 
 setInterval(countMinutes, 1000);
 
 setInterval(() => {
   const date = new Date();
-  const hour = (date.getHours()) % 12;
+  const hour = date.getHours() % 12;
 
   let hourDegree = (hour / 12) * 360 + 90;
   hourHand.style.transform = `rotate(${hourDegree}deg)`;
-  digitalHour.innerHTML = hour + ":";
 
+  
+  let myScreenWidth = window.screen.width;
+  if (myScreenWidth > 240) {
+    digitalHour.innerHTML = hour.toString().padStart(2, "0") + ":";
+  } else {
+    digitalHour.innerHTML = hour.toString().padStart(2, "0");
+  }
 }, 1000);
